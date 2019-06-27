5b74763d9e44d2e40c83b434b52fb05c3433bd21
root <root@localhost.localdomain>
2019-06-27 09:45:11 -0400

    port to python3.6

--- README.md
+++ README.md
@@ -1,8 +1,36 @@
-pythonServ.py is a python socket server listening on 9999 port and it will receive cmds from the 9999 port and spawn a thread to run the cmd locally and send back the cmd output.
+pythonServ.py is a python socket server listening on 9999 port and it will receive remote cmds from the 9999 port and spawn a thread to execute the cmd locally and send back the cmd output to remote. This is used for frontend to execute the backend application in the CCDIR directory and read the output after the execution.
+This is a quick rollout so there is no Exception handling at the moment. Will add the Exception handling later.
 
-0_generateContactFromXML.py, 1_makeAgents.py, 2_filterSpam, 3_impticket are used for restore the freshdesk tickets from its XML backups via the freshdesk restful APIs.
+Example (run 'ls' on remote machine and get the output back):
 
-retriveOrder.py is a sample program that could retrieve the order details from the cubecart mysql database and output the .csv formated order data.
+1.make sure you have '/u/ccr.14/lib' '/u/ccr.14/std/binl/' directory;
+2.cp your 'ls' program into '/u/ccr.14/std/binl/' directory;
+3.in Terminal 1, run './pythonServ.py 192.168.1.106 9999'
+  You will see 'Server listening on 192.168.1.106 9999'
+4.in the other machine, you can write your own programs to connect to socket port 9999 to send/recv cmd and output. Or you can use 'telnet 192.168.1.106 9999' to connect and issue 'ls' command.
+Sample Outputs:
+-------------------------------------------------------------------
+[root@localhost pythonServer]# telnet 192.168.1.106 9999
+Trying 192.168.1.106...
+Connected to 192.168.1.106.
+Escape character is '^]'.
+ls
+pythonServ.py
+README.md
+Connection closed by foreign host.
+--------------------------------------------------------------------
+[root@localhost pythonServer]# telnet 192.168.1.106 9999
+Trying 192.168.1.106...
+Connected to 192.168.1.106.
+Escape character is '^]'.
+ls -lrt
+total 8
+-rw-r--r-- 1 root root  329 Jun 27 09:12 README.md
+-rwxrwxr-x 1 root root 1805 Jun 27 09:37 pythonServ.py
+Connection closed by foreign host.
+------------------------------------------------------------------
 
-Mark Miao1 from mastera
+
+
+
 
--- pythonServ.py
+++ pythonServ.py
@@ -1,6 +1,5 @@
-#!/usr/bin/python
-
-from thread import *
+#!/usr/bin/python3.6
+from threading import *
 import socket
 import sys
 from subprocess import Popen, PIPE
@@ -10,7 +9,7 @@ def clientthread(conn,addr):
     reply = ""
     cmd = ""
     threadID=str(get_ident())
-    print threadID+':Accept Connection from ' + addr[0] + ':' + str(addr[1])
+    print(threadID+':Accept Connection from ' + addr[0] + ':' + str(addr[1]))
     buffer=""
 
 	#setup the ccdir and ld library
@@ -21,43 +20,49 @@ def clientthread(conn,addr):
 
 	#read the command from the web perl
     while True:
-		buffer = conn.recv(1024)
-		if len(buffer)>0:
-			print threadID+':Received STRING: '+buffer
-			break
+        ByteBuffer = conn.recv(1024)
+        buffer = ByteBuffer.decode("utf-8")
+        buffer = buffer.rstrip()
+        if len(buffer)>0:
+            print(threadID+':Received STRING: '+buffer)
+            break
 
 	#parse the cmd
     cmd = buffer.split(" ")[0]
     args = " ".join(buffer.split(" ")[1:])
 
-    print threadID+":CMD:"+binPath+cmd
-    print threadID+":ARGS:"+args
+    print(threadID+":CMD:"+binPath+cmd)
+    print(threadID+":ARGS:"+args)
 
 	#execute the control cmd
-    process = Popen([binPath+cmd,args], stdout=PIPE)
-    (output, err) = process.communicate()
+    process = Popen([binPath+cmd+" "+args], stdout=PIPE, shell=True)
+    (ByteOutput, err) = process.communicate()
     exit_code = process.wait()
-    print threadID+":exit code:"+str(exit_code)
-    print threadID+":output:"+output
-    print threadID+":err:"+str(err)
+    output = ByteOutput.decode("utf-8")
+    print(threadID+":exit code:"+str(exit_code))
+    print(threadID+":output:"+output)
+    print(threadID+":err:"+str(err))
     
-    if(len(output)):
-        reply = output
+    #if(len(output)):
+    reply = output.encode('utf-8')
 
 	#send back the output and exit this thread.
     conn.sendall(reply)
     conn.close()
 
-host = 'sam'
-port = 9999
+#host = 'sam'
+host = sys.argv[1]
+port = int(sys.argv[2])
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
 s.bind((host, port))
 s.listen(128)
-print "Server listening on %s %d" %(host, port)
+print("Server listening on %s %d" %(host, port))
 while(1):
-	conn, addr = s.accept()
-	start_new_thread(clientthread ,(conn,addr))
+    conn, addr = s.accept()
+    #start_new_thread(clientthread ,(conn,addr))
+    Thread(target=clientthread,
+        args=(conn,addr),).start()
 s.close()
 
 
